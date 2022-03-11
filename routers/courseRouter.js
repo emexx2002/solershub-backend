cd5bfc8f457e76abe40f88c452f209ff3ec3660b
Eric Alaribe <ericsmithsonian2@gmail.com>
2022-03-12 00:43:06 +0100

    Edited courseRouter.js

--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -1,5 +1,21 @@
 // courseRouter.js - courses route model
-var express = require('express')
-var Router = express.Router()
+const express = require('express')
+const router = express.Router()
+const Course = require('../models/courseModel')
 
 //Course page route
+router.post('/addnewcourse', (res, req) => {
+    res.send("New course(s) added.")
+})
+
+//Add new courses
+exports.courseinstance_create_get = function (req, res) {
+    res.send('New course added!')
+}
+
+//Delete courses
+exports.courseinstance_delete_get = function (req, res) {
+    res.send('Course deleted!')
+}
+
+module.exports = router
