f1fb7acf7e643f0bcf0dcf5cbcfc343d3c100c4e
Hebron Praise <panampraisehebron@gmail.com>
2022-03-12 02:19:26 +0100

    Updated routers

--- routers/instructorRouter.js
+++ routers/instructorRouter.js
@@ -1,6 +1,5 @@
 // courseRouter.js - courses route model
 const express = require('express')
-const Instructor = require('../models/instructorModel')
 const authController = require('../controllers/authController')
 
 const router = express.Router()
