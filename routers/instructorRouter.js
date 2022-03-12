43cf1d120890cd293509f3c2f72dcdf5ec65c963
Eric Alaribe <ericsmithsonian2@gmail.com>
2022-03-12 01:29:42 +0100

    merge changes

---
+++ routers/instructorRouter.js
@@ -0,0 +1,6 @@
+// courseRouter.js - courses route model
+const express = require('express')
+const app = express()
+const Instructor = require('../models/instructorModel')
+
+module.exports = router
