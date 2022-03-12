dc5dcbcd7d7ddc46d329dec966871dbf8a7a38b4
Eric Alaribe <ericsmithsonian2@gmail.com>
2022-03-12 03:27:24 +0100

    Edited email.js file

--- utils/email.js
+++ utils/email.js
@@ -6,15 +6,30 @@ class Email{
     constructor(API_KEY, mailto) {
         this.API_KEY = API_KEY
         this.mailto = mailto
+        this.mailfrom = this.mailfrom
     } 
-    sendWelcome(subject) {
+    sendWelcome(subject, url) {
         sgMail.setApiKey(this.API_KEY)
         to = this.mailto
-        from = 'solershub@gmail.com'
+        from = this.mailfrom
         subject = "Welcome!"
-        text = 'Congratulations on creating an account with SolersHub'
-        html = ''
+        text = 'Congratulations on creating an account with SolersHub. \
+        Please, confirm your mail here.'
         .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+    sendPasswordReset(url, time) {
+        sgMail.setApiKey(this.API_KEY)
+        to = this.mailtofrom
+        from = this.mailfrom
+        subject = "Password reset link"
+        html = "Here's the link to reset your password."
+                .send(msg)
         .then(() => {
             console.log('Email sent')
         })
